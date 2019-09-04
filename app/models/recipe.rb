a81725782d02a707810993382e0ed6cc23606683
fromwhence <wenc.matt@gmail.com>
2019-09-04 17:01:33 -0500

    end of day

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,13 +7,37 @@ class Recipe < ApplicationRecord
     directions.split(", ")   
   end
 
+  def friendly_created_at
+    created_at.strftime('%b %d, %Y') 
+  end
+
   def formatted_prep_time
     # input: 125
     # output: "2 Hours, 5 Minutes"
     # find 
-
     hours = prep_time / 60
     minutes = prep_time % 60
-    "#{hours} Hours, #{minutes} Minutes."
+    time_string = ""
+
+  
+    time_string += "#{hours} #{'Hour'.pluralize(hours)}" if hours > 0
+
+    # if hours > 0
+    #   time_string += "#{hours} #{'Hour'.pluralize(hours)}"
+    # end
+
+    time_string += ", " if hours > 0 && minutes > 0
+
+    # if hours > 0 && minutes > 0
+    #   time_string += ", "
+    # end
+
+    time_string += "#{minutes} #{'Minute'.pluralize(minutes)}" if minutes > 0
+
+    # if minutes > 0
+    #   time_string += "#{minutes} #{'Minute'.pluralize(minutes)}"
+    # end
+
+    time_string
   end
 end
